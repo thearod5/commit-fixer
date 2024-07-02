0a0dee65d56d5037a7ebe596bcaa87f983162053
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-07-02 11:45:36 -0500

    Adding docs to script file
    - Improved code documentation by adding docstrings to several functions, clarifying their purpose, parameters, and return values.

--- commit_fixer.py
+++ commit_fixer.py
@@ -17,24 +17,33 @@ INSTRUCTIONS_PROMPT = (
     "Each change should group the diffs into a behavioral change."
 )
 
-example_json = {
+EXAMPLE_JSON = {
     "diffs": ["diff desc 1", "diff desc 2"],
     "changes": ["change desc 1", "change desc 2"]
 }
 
 
 def get_format_prompt() -> str:
-    example_json_str = json.dumps(example_json, indent=4)
+    """
+    Generates prompt displaying the expected format of the json response.
+    :return: Format prompt as string.
+    """
+    example_json_str = json.dumps(EXAMPLE_JSON, indent=4)
     return f"Output valid json like so:\n```json\n{example_json_str}\n```"
 
 
-def generate_summary(commit_message):
+def generate_summary(commit_diff: str):
+    """
+    Summarizes the changes present in the commit diff.
+    :param commit_diff: String containing all the diffs of commit.
+    :return: Summary of commit diffs.
+    """
     format_prompt = get_format_prompt()
     system_prompt = "\n\n".join([INSTRUCTIONS_PROMPT, format_prompt])
     llm_manager = get_llm_manager()
     response = llm_manager.invoke([
         ("system", system_prompt),
-        ("human", commit_message)
+        ("human", commit_diff)
     ]).content
     start_index = response.find("```json")
     end_index = response.find("```", start_index + 1)
@@ -49,6 +58,10 @@ def generate_summary(commit_message):
 
 
 def get_llm_manager():
+    """
+    Reads LLM manager from env variables.
+    :return:
+    """
     llm_manager_type = os.environ["LLM_MANAGER"]
     assert llm_manager_type in ALLOWED_MANAGERS, f"Unrecognized manager `{llm_manager_type}`"
     llm_key = os.environ["LLM_KEY"]
@@ -59,11 +72,21 @@ def get_llm_manager():
 
 
 def change_to_message(change_messages: List[str]):
+    """
+    Formats list of changes as a Markdown list.
+    :param change_messages: List of diff summaries.
+    :return: String delimited changes as a Markdown list.
+    """
     content = '\n'.join(["- " + c for c in change_messages])
     return content
 
 
 def read_file(f_path):
+    """
+    Reads file at given path.
+    :param f_path: Path to the file to read.
+    :return: Content of the file.
+    """
     with open(f_path, 'r') as file:
         f_content = file.read()
         return f_content
