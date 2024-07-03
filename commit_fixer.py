4b87a2330a8d528650afd4f7c5b9120a13320ac0
Alberto Daniel Rodriguez <arodri39@nd.edu>
2024-07-03 09:59:40 -0500

    Refactor commit_fixer.py to summarize staged changes and integrate with SAFA projects for artifact retrieval.
    - The commit_fixer.py script has been significantly refactored. The functionality to automatically summarize commit changes using a language model has been removed. Instead, the script now supports interactively editing the title and changes for a commit. It prompts the user to enter a new title, edit existing changes, or add new changes. The user can then commit the changes with the updated title and change descriptions.
    - The script now uses the git library to interact with the Git repository and retrieve staged changes for files. It creates a mapping of file paths to their diffs and content summaries based on project data fetched from a Safa client.
    - The language model integration has been replaced with a simplified function to generate a JSON response containing a title and change descriptions for the staged file changes. This JSON is then used to populate the interactive commit message editing interface.
    - Various utility functions have been added or modified to support the new interactive commit message editing workflow, such as converting changes to a formatted message string and printing the updated commit message.

--- commit_fixer.py
+++ commit_fixer.py
@@ -1,8 +1,9 @@
 import json
 import os
 import sys
-from typing import List
+from typing import Dict, List, Tuple
 
+import git
 from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
 from langchain_community.llms import OpenAI
@@ -13,149 +14,75 @@ ALLOWED_MANAGERS = {
     "anthropic": lambda k: ChatAnthropic(anthropic_api_key=k, model_name='claude-3-sonnet-20240229'),
     "openai": lambda k: OpenAI(openai_api_key=k)
 }
-INSTRUCTIONS_PROMPT = (
-    "Produce the JSON output to summarize the given commit changes. "
-    "Each diff should be converted to natural language. "
-    "Each change should group the diffs into a behavioral change."
+TITLE_INSTRUCTIONS = (
+    "Create title describing how the code is changing."
 )
 
-SUMMARIZE_FORMAT_JSON = {
-    "diffs": ["diff desc 1", "diff desc 2"]
+TITLE_FORMAT = {
+    "title": "commit title"
 }
 
-SYNTHESIZE_FORMAT_JSON = {
+SUMMARIZE_INSTRUCTIONS = (
+    "The user will present a file from their software project. "
+    "This file has been modified and its your job to describe how the system is changing. "
+    "Each file changed will also contain a summary of what it used to accomplish in the system and "
+    "your job is to describe how it is changing. "
+    "Produce JSON describing each file level change followed by a title of the overall set of changes.."
+)
+
+SUMMARIZE_FORMAT = {
     "changes": ["change desc 1", "change desc 2"],
     "title": "commit title"
 }
 
 
-def run_menu(state: Dict, menu: Dict[str, Callable]):
-    commit = state["commit"]
-    title, changes = split_commit_message(commit.message)
-    print_commit("Commit", title, changes=changes)
-    selected_key = get_menu_option(menu.keys())
-    menu_action = menu[selected_key]
-    menu_action(state)
-
-
-def get_format_prompt() -> str:
-    """
-    Generates prompt displaying the expected format of the json response.
-    :return: Format prompt as string.
-    """
-    example_json_str = json.dumps(EXAMPLE_JSON, indent=4)
-    return f"Output valid json like so:\n```json\n{example_json_str}\n```"
-
-
-def generate_commit_summary(state: Dict):
-    repo = state["repo"]
-    commit = state["commit"]
-    safa_client = state["safa_client"]
-
-    commit_diff = get_commit_diff(repo, commit)
-    title, changes = generate_summary(commit_diff)
-    print_commit("Generated", title, changes=changes)
-    # Next
-    run_menu(state, MAIN_MENU)
-
-
-def edit_title(state: Dict):
-    commit_message = state["message"]
-    title, changes = split_commit_message(commit_message)
-    new_title = input("New Title > ")
-    if new_title == "":
-        print("Skipped")
-        return None
-    # Next
-    state["message"] = combine_commit_message(new_title, changes)
-    op_amend(state)
-    run_menu(state, MAIN_MENU)
-
-
-def edit_changes(state: Dict):
-    commit_message = state["message"]
-    title, changes = split_commit_message(commit_message)
-    changes_kept = []
-    for change in changes:
-        print("-" * LINE_LENGTH)
-        edit_change = get_yn(change, title="\nEdit/Delete this change?")
-
-        if edit_change:
-            new_change_desc = input("New Change (empty for remove):")
-            if new_change_desc == "":
-                print("Removed.")
-                continue
-            changes_kept.append(new_change_desc)
-        else:
-            changes_kept.append(change)
-            print("Keeping as is.")
-    # Next
-    state["message"] = combine_commit_message(title, changes_kept)
-    op_amend(state)
-    run_menu(state, MAIN_MENU)
-    print("Changes have been saved.\n\n")
-
-
-def get_yn(p: str, title: str = None):
-    if title:
-        print(title, "\n")
-
-    print(p)
-    answer = input("\n(y/n) >")
-    valid_answers = {"y", "n"}
-    if answer not in valid_answers:
-        return get_yn(p, title=title)
-    return answer == "y"
-
-
-def split_commit_message(commit_message: str) -> Tuple[str, List[str]]:
-    commit_title, *commit_changes = commit_message.split("\n\n")
-    if len(commit_changes) > 0:
-        commit_changes = [c.replace("- ", "") for c in commit_changes[0].split("\n") if len(c) > 0]
-    return commit_title, commit_changes
-
-
-def combine_commit_message(title: str, changes: List[str]) -> str:
-    content = change_to_message(changes)
-    if content == "":
-        return title
-    return f"{title}\n\n{content}"
-
-
-"""
-Operations
-- Amend
-- Continue
----
-"""
-
-
-def op_amend(state):
-    if "message" not in state:
-        return
-    repo = state["repo"]
-    commit = state["commit"]
-    commit_message = state["message"]
-    repo.git.commit('--amend', '-m', commit_message, '--no-edit', '--author', commit.author)
-
-
-def generate_summary(commit_message):
-    system_prompt = "\n\n".join([INSTRUCTIONS_PROMPT, FORMAT_PROMPT])
-    llm_manager = get_llm_manager()
-    response = llm_manager.invoke([
+def generate_diff_summary(changes):
+    prompt = create_change_prompt(changes)
+    system_prompt = "\n\n".join([SUMMARIZE_INSTRUCTIONS, get_format_prompt(SUMMARIZE_FORMAT)])
+    messages = [
         ("system", system_prompt),
-        ("human", commit_diff)
-    ]).content
+        ("human", prompt)
+    ]
+    print("...generating...")
+    response_json = generate_json(messages)
+    diff_summaries = response_json["changes"]
+    title = response_json["title"]
+    return title, diff_summaries
+
+
+def create_change_prompt(changes: List[Dict]) -> str:
+    prompt = ""
+
+    for change in changes:
+        change_prompt = []
+        change_prompt.append(f"# File: {change['file']}")
+        change_prompt.append(f"## Functionality before change\n{change['summary']}")
+        change_prompt.append(f"## Changes\n{change['diff']}")
+        prompt += "\n\n".join(change_prompt)
+
+    return prompt
+
+
+def generate_json(messages: List[Tuple]):
+    llm_manager = get_llm_manager()
+    response = llm_manager.invoke(messages).content
     start_index = response.find("```json")
     end_index = response.find("```", start_index + 1)
     json_str = response[start_index + 7:end_index]
     try:
-        json_dict = json.loads(json_str)
-        change_summaries = json_dict["changes"]
-        return change_to_message(change_summaries)
+        return json.loads(json_str)
     except Exception as e:
         print(response)
         raise e
+
+
+def get_format_prompt(example_json) -> str:
+    """
+    Generates prompt displaying the expected format of the json response.
+    :return: Format prompt as string.
+    """
+    example_json_str = json.dumps(example_json, indent=4)
+    return f"Output valid json like so:\n```json\n{example_json_str}\n```"
 
 
 def get_llm_manager():
@@ -172,7 +99,7 @@ def get_llm_manager():
     return llm_manager
 
 
-def change_to_message(change_messages: List[str]):
+def changes_to_message(change_messages: List[str]):
     """
     Formats list of changes as a Markdown list.
     :param change_messages: List of diff summaries.
@@ -188,42 +115,83 @@ def get_staged_diffs(repo):
     """
     staged_files = [item.a_path for item in repo.index.diff("HEAD")]
 
-    diffs = []
+    file2diff = {}
     for file in staged_files:
         diff = repo.git.diff('HEAD', file)
-        diffs.append(diff)
+        file2diff[file] = diff
 
-    return diffs
+    return file2diff
+
+
+def create_artifact_map(artifacts: List[Dict]):
+    artifact_map = {}
+    for artifact in artifacts:
+        artifact_map[artifact["name"]] = artifact
+    return artifact_map
+
+
+def to_commit_message(title, changes):
+    return f"{title}\n\n{changes_to_message(changes)}"
+
+
+def print_commit_message(title, changes):
+    print(to_commit_message(title, changes))
 
 
 if __name__ == "__main__":
-    MAIN_MENU = {
-        "Generate": generate_commit_summary,
-        "Edit Title": edit_title,
-        "Edit Changes": edit_changes,
-        "Keep as is": lambda s: None
-    }
+
     repo_path: str = "."
     load_dotenv()
 
     email = os.environ["SAFA_EMAIL"]
     password = os.environ["SAFA_PASSWORD"]
     version_id = os.environ["SAFA_VERSION_ID"]
+    cache_file_path = os.environ["CACHE_FILE_PATH"]
 
-    CACHE_FILE_PATH = os.path.expanduser("~/projects/commit-fixer/local.cache")
-    client_store = SafaStore(CACHE_FILE_PATH)
+    client_store = SafaStore(os.path.expanduser(cache_file_path))
     client = Safa(client_store)
     client.login(email, password)
     project_data = client.get_project_data(version_id)
+    artifact_map = create_artifact_map(project_data["artifacts"])
 
-    r = git.Repo(repo_path)
-    diffs = get_staged_diffs(r)
+    repo = git.Repo(repo_path)
+    file2diff = get_staged_diffs(repo)
+    if len(file2diff) == 0:
+        print("No changes staged for commit.")
+        sys.exit(0)
 
-    commit_state = {
-        "repo": r,
-        "commit": c,
-        "message": c.message,
-        "safa_client": client,
-        "version_id": version_id
-    }
-    run_menu(commit_state, MAIN_MENU)
+    changes = []
+    for file, diff in file2diff.items():
+        file_artifact = artifact_map[file]
+        changes.append({
+            "file": file,
+            "diff": diff,
+            "summary": file_artifact["summary"] if file_artifact["summary"] != "" else file_artifact["content"]
+        })
+
+    title, changes = generate_diff_summary(changes)
+
+    menu_options = ["Edit title", "Edit changes", "Add change", "Commit"]
+    running = True
+
+    while running:
+        print_commit_message(title, changes)
+        for i, menu_option in enumerate(menu_options):
+            print(f"{i + 1})", menu_option)
+        option = input(">")
+        option_num = int(option)
+
+        if option_num == 1:
+            title = input("New Title:")
+        elif option_num == 2:
+            change_num = int(input("Change Number"))
+            changes[change_num - 1] = input("New Change:")
+        elif option_num == 3:
+            changes.append(input("New Change:"))
+        elif option_num == 4:
+            repo.index.commit(to_commit_message(title, changes))
+            running = False
+        else:
+            raise Exception("Invalid option")
+
+    print("Done.")
